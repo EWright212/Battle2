1a2f102352d73e44f653386de9e89358bec3f899
Student <emilywright212@gmail.com>
2020-02-07 14:07:10 +0000

    Refactor with game class

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require "./lib/player"
 require './lib/game'
 
 class Battle < Sinatra::Base
-  # set :session_secret, "nah nah nah"
+  #  set :session_secret, "nah nah nah"
 
   enable :sessions
 
@@ -12,21 +12,24 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
   
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @game = $game
+    @player_1 = @game.player_1
+    @player_2 = @game.player_2
     erb :play
   end
 
   get '/attack' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    Game.new.attack(@player_2)
+    @game = $game
+    @player_1 = @game.player_1
+    @player_2 = @game.player_2
+    @game.attack(@player_2)
     erb :attack
   end
   #  start the server if the ruby file executed properly
